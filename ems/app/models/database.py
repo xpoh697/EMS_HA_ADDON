ac1e01bceccf0a234912f73226dbb8c9b4d3cfaa
Vitaly Cherenko <xpoh697@gmail.com>
2026-04-09 16:15:54 +0300

    Fix ImportError: remove create_all from sqlalchemy imports

--- ems/app/models/database.py
+++ ems/app/models/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_all, Column, Integer, String, Float, DateTime, Boolean, JSON
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
